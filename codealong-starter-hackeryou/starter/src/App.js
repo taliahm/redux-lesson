5f71a1e270cb12c169d00a74804b9debb26d6b91
Talia Harrison-Marcassa <taliaharrisonmarcassa@Junos-MacBook-Pro.local>
2020-02-24 11:23:33 -0500

    adding npm starter files

---
+++ codealong-starter-hackeryou/starter/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import IcebergCounter from './IcebergCounter.js';
+import Footer from './components/Footer';
+
+import reducer from './connect/reducer';
+
+import './App.css';
+// Create global state here, in a larger app you would pass in a combination of all your reducers. 
+// You do this using combineReducers
+const store = createStore(reducer);
+
+function App() {
+  return (
+    <Provider store={store} >
+      <div className="wrapper">
+        <div className="hold-counter">
+         <IcebergCounter />
+        </div>
+      </div>
+      <Footer/>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ codealong-starter-hackeryou/starter/src/connect/reducer.js
@@ -0,0 +1,7 @@
+// We will create a reducer that uses a switch statement to evaluate different TYPES and updates state based on those types.
+
+
+const icebergReducer = () => {
+}
+
+export default icebergReducer;
