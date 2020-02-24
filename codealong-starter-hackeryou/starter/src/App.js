14ac21501da31d9f6db0af0ea8986337f00e2286
Talia Harrison-Marcassa <taliaharrisonmarcassa@Junos-MacBook-Pro.local>
2020-02-24 12:28:28 -0500

    adds redux dev tools

--- codealong-starter-hackeryou/starter/src/App.js
+++ codealong-starter-hackeryou/starter/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import IcebergCounter from './IcebergCounter.js';
 import Footer from './components/Footer';
 
 import reducer from './connect/reducer';
 
 import './App.css';
+
 // Create global state here, in a larger app you would pass in a combination of all your reducers. 
 // You do this using combineReducers
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 function App() {
   return (
